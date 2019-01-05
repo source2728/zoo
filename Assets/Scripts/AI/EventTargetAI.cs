a6735df60755ea5485141d81539788eadcae7148
source2728 <source2728@outlook.com>
2019-01-05 15:22:14 +0800

    123

---
+++ Assets/Scripts/AI/EventTargetAI.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class EventTargetAI : SimpleAI
+{
+    private long AppearStartTime;
+    private const long TotalAppearTime = 20;
+    private const float DisappearIdleTime = 2f;
+
+    void Start()
+    {
+        MoveTimePreGrid = 2f;
+        Agent = GetComponent<SceneObject>();
+        AppearStartTime = TimeUtil.CurrentTime();
+        RandAppear();
+        RandMove();
+    }
+
+    protected override void OnMoveToGridFinish()
+    {
+        var appearTime = TimeUtil.CurrentTime() - AppearStartTime;
+        if (appearTime <= TotalAppearTime)
+        {
+            CurGrid = MoveTargetGrid;
+            RandMove();
+        }
+        else
+        {
+            Agent.DoIdle();
+            var sequence = DOTween.Sequence();
+            sequence.AppendInterval(DisappearIdleTime);
+            sequence.AppendCallback(OnDisappear);
+        }
+    }
+}
